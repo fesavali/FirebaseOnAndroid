2d694ca302d39f645b1befe3d8fa4fd4555d0390
fesavali <felixnzioki99@gmail.com>
2021-11-12 16:19:52 +0300

    create login activity

---
+++ app/src/main/java/com/savaliscodes/firebaseonandroid/Login.kt
@@ -0,0 +1,55 @@
+package com.savaliscodes.firebaseonandroid
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val auth = FirebaseAuth.getInstance()
+        if(auth.currentUser != null){
+            val intent =  Intent(this, MainActivity::class.java)
+            intent.putExtra(USER_ID, auth.currentUser!!.uid)
+            startActivity(intent)
+        }
+        val btnSignIn = findViewById<Button>(R.id.signIn)
+        btnSignIn.setOnClickListener {
+            val providers = arrayListOf(
+               AuthUI.IdpConfig.EmailBuilder().build()
+            )
+            startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(), RC_SIGN_IN)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == RC_SIGN_IN){
+            val response = IdpResponse.fromResultIntent(data)
+            if(resultCode == Activity.RESULT_OK){
+                val user = FirebaseAuth.getInstance().currentUser
+                val intent = Intent(this, MainActivity::class.java)
+                intent.putExtra(USER_ID, user!!.uid)
+                startActivity(intent)
+            }else{
+                Toast.makeText(this, "Login operation failed", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+    }
+
+    companion object {
+        const val USER_ID = "user_id"
+        const val RC_SIGN_IN = 15
+    }
+}
