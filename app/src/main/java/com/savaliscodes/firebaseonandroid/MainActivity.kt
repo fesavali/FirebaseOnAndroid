14ef4fa6524bdfb6ef8cbd6bf4ed4b150be16041
fesavali <felixnzioki99@gmail.com>
2021-11-12 16:21:59 +0300

    display user

--- app/src/main/java/com/savaliscodes/firebaseonandroid/MainActivity.kt
+++ app/src/main/java/com/savaliscodes/firebaseonandroid/MainActivity.kt
@@ -2,11 +2,29 @@ package com.savaliscodes.firebaseonandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //implement firebase auth
+        val auth = FirebaseAuth.getInstance()
+        val userID:String = intent.getStringExtra("user_id").toString()
+        if(userID != null){
+            val tvUser = findViewById<TextView>(R.id.user)
+            val user = auth.currentUser?.displayName.toString()
+            tvUser.text = user
+        }else{
+            Toast.makeText(this,"No User Found", Toast.LENGTH_SHORT).show()
+        }
+
+        val tvID = findViewById<TextView>(R.id.id)
+
+        tvID.text = userID
     }
-    //implement firebase auth
+
+
 }
