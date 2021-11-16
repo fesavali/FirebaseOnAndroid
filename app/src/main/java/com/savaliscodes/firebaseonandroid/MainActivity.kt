cc999d2c182b60c1e6485147102f3c9d07a13ce5
fesavali <felixnzioki99@gmail.com>
2021-11-16 16:29:41 +0300

    check if user is anonymous

--- app/src/main/java/com/savaliscodes/firebaseonandroid/MainActivity.kt
+++ app/src/main/java/com/savaliscodes/firebaseonandroid/MainActivity.kt
@@ -1,15 +1,28 @@
 package com.savaliscodes.firebaseonandroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.savaliscodes.firebaseonandroid.Login.Companion.SIGN_MESSAGE
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        //check if user is logged in
+        val user = FirebaseAuth.getInstance().currentUser
+        if(user == null || user.isAnonymous){
+            val intent =  Intent(this, AnonymousUser::class.java)
+            intent.putExtra(SIGN_MESSAGE, "signIn to view this page")
+            startActivity(intent)
+        }
+
+
         //implement firebase auth
         val auth = FirebaseAuth.getInstance()
         val userID:String = intent.getStringExtra("user_id").toString()
@@ -25,6 +38,8 @@ class MainActivity : AppCompatActivity() {
 
         tvID.text = userID
     }
-
+companion object{
+    const val ATTEMPT_SIGN = 10
+}
 
 }
